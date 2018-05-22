554380e0fb9664f2fd476ca72facfb70d7118120
Alex Lerner <lernera@thomsonreuters.com>
2018-05-22 15:06:53 -0400

    finished linked list

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -11,6 +11,8 @@ class LinkedList {
     }
 
     insertFirst(data) {
+        // insertAt(data, 0)
+
         const node = new Node(data);
 
         if (this.head === null)
@@ -22,8 +24,9 @@ class LinkedList {
     }
 
     size() {
-        if (this.head === null)
+        if (this.head === null) {
             return 0;
+        }
 
         let count = 0;
         let node = this.head;
@@ -36,19 +39,25 @@ class LinkedList {
     }
 
     getFirst() {
+        // getAt(0)
         return this.head;
     }
 
     getLast() {
-        if (this.head === null)
-            return this.head;
+        // getAt(this.size() - 1)
 
-        let node = this.head;
-        while (node.next !== null) {
-            node = node.next;
+        if (!this.head) {
+            return null;
         }
 
-        return node;
+        let node = this.head;
+        while (node) {
+            if (!node.next) {
+                return node;
+            }
+
+            node = node.next;
+        }
     }
 
     clear() {
@@ -56,16 +65,18 @@ class LinkedList {
     }
 
     removeFirst() {
-        if (this.head === null)
+        if (this.head === null) {
             return;
+        }
 
         let node = this.head;
         this.head = this.head.next;
     }
 
     removeLast() {
-        if (this.head === null)
+        if (this.head === null) {
             return;
+        }
 
         if (this.head.next === null) {
             // this is a one node list.  So just kill the head.
@@ -98,7 +109,76 @@ class LinkedList {
     }
 
     getAt(index) {
+        // from this method, getFirst() is getAt(0)
+        let counter = 0;
+        let node = this.head;
+        while (node) {
+            if (counter == index) {
+                return node;
+            }
+
+            counter++;
+            node = node.next;
+
+        }
+
+        // if got to this point, node is null and we didnt get to the index
+        // the caller was asking for
+        return null;
+    }
+
+    removeAt(index) {
+        if (!this.head) {
+            return;
+        }
+
+        if (index ==0) {
+            this.head = this.head.next;
+            return;
+        }
+
+        const previous = this.getAt(index - 1);
+        if (!previous || !previous.next) {
+            return;
+        }
+        previous.next = previous.next.next;
+    }
+    
+    insertAt(data, index) {
+        // from this method, insertFirst() is insertAt(0)
         
+        if (!this.head) {
+            this.head = new Node(data);
+            return;
+        }
+
+        if (index === 0) {
+            this.head = new Node(data, this.head);
+            return;
+        }
+
+        // if index out of bounds, add it to the end
+        const previous = this.getAt(index - 1) || this.getLast();
+        const node = new Node(data, previous.next);
+        previous.next = node;
+    }
+
+    forEach(func) {
+        let count = 0;
+        let node = this.head;
+        while (node) {
+            func(node, count);
+            count++;
+            node = node.next;
+        }
+    }
+
+    *[Symbol.iterator]() {
+        let node = this.head;
+        while (node) {
+            yield node;
+            node = node.next;
+        }
     }
 }
 
