bd0554d4f94513f0f41240d2fb5f1f550e26e571
Alex Lerner <alernerdev@gmail.com>
2018-05-23 20:16:26 -0400

    added memoization on fibonacci

--- fib/index.js
+++ fib/index.js
@@ -9,24 +9,50 @@
 //   fib(4) === 3
 
 
-function fib(n) {
-    const result = [0,1];
-
-    for (let i=2; i<=n; i++) {
-        let a = result[i-1];
-        let b = result[i-2];
-        result.push(a+b);
-    }
-
-    return result[result.length - 1];
-}
-
 // function fib(n) {
-//     if (n < 2) {
-//         return n;
+//     const result = [0,1];
+
+//     for (let i=2; i<=n; i++) {
+//         let a = result[i-1];
+//         let b = result[i-2];
+//         result.push(a+b);
 //     }
 
-//     return ( fib(n-1) + fib(n-2) );
+//     return result[result.length - 1];
 // }
 
+/* by itself, recursive fib is much slower than the normal iterative approach because it calclulates the same
+numbers multiple times.
+Memoization is recording inputs and outputs for a function -- and caching them
+*/
+
+function memoize(fn) {
+    let cache = {}
+
+    // return the fast function with cached values
+    return function(...args) {
+        // have I already seen these arguments ?
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        // let the original function flow proceed.  call the original function
+        const result = fn.apply(this, args);
+        cache[args] = result;
+ 
+        return result;
+    }
+
+}
+
+function slowFib(n) {
+     if (n < 2) {
+         return n;
+     }
+
+     // NOTICE THAT ITS RECURSING THE FAST MEMOIZED FUNCTION
+     return ( fib(n-1) + fib(n-2) );
+}
+
+const fib = memoize(slowFib);
 module.exports = fib;
